42a4b594ec7ae411ce51aaa9be2d25e1a63dad04
Anastasia G <asya_apto@mail.ru>
2021-11-13 23:00:03 +0300

    Make changes according to reviewer's suggestions

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import argparse
+import datetime as dt
+
+import pandas
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import datetime as dt
-import pandas
-import openpyxl
+
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Запуск сайта по продаже вина. Подключает файл с данными для сайта.'
+    )
+    parser.add_argument('--data_path',
+                        help='Путь к файлу с данными wines.xlsx',
+                        default='wines.xlsx')
+    args = parser.parse_args()
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
@@ -12,31 +23,30 @@ def main():
 
     template = env.get_template('template.html')
 
-
     current_year = dt.datetime.now().year
-    current_age = current_year - 1920
+    founding_year = 1920
+    current_age = current_year - founding_year
     last_digit = str(current_age)[-1]
 
     if last_digit == "1":
-        word_years = "год"
+        years_word = "год"
     elif last_digit == "2" or last_digit == "3" or last_digit == "4":
-        word_years = "года"
+        years_word = "года"
     else:
-        word_years = "лет"
+        years_word = "лет"
 
-
-    wines_df = pandas.read_excel('wine3.xlsx', keep_default_na=False)
+    wines_df = pandas.read_excel(args.data_path, keep_default_na=False)
     categories = sorted(set(wines_df['Категория'].to_list()))
 
-    data_beverages = {}
+    all_beverages = {}
     for category in categories:
-        data_beverages[category] = wines_df.loc[wines_df['Категория'] == category].to_dict('records')
+        all_beverages[category] = wines_df.loc[wines_df['Категория'] == category].to_dict('records')
 
 
     rendered_page = template.render(
         current_age=current_age,
-        word_years=word_years,
-        all_wines=data_beverages
+        word_years=years_word,
+        all_wines=all_beverages
     )
 
     with open('index.html', 'w', encoding='utf8') as file:
@@ -44,6 +54,7 @@ def main():
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
 
 if __name__ == "__main__":
     main()
